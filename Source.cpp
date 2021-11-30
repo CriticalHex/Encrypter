dc0b829d346c06250137319678a8fc487055ff05
Email 6 <loserkid0006@outlook.com>
2021-11-30 14:05:02 -0700

    Finished Product?

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 #include<algorithm>
 #include<string>
 #include<cmath>
+#include<Windows.h>
 
 int main() {
 	srand(time(NULL));
@@ -14,28 +15,34 @@ int main() {
 
 	char letters[25] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	char gridArray[25];
+	char gridArray[5][5];
 
 	std::cout << "Enter '0' to encode a message, '1' to decode: ";
 	std::cin >> EDSwitch;
 
 	if (EDSwitch == 0) { //grid creation, only when encoding
-		for (int i = 0; i < 25; i++) {
-			int letter = rand() % 25;
-			while (letters[letter] == NULL) {
-				letter = rand() % 25;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				int letter = rand() % 25;
+				while (letters[letter] == NULL) {
+					letter = rand() % 25;
+				}
+				//std::cout << letter << "\n";
+				gridArray[i][j] = letters[letter];
+				letters[letter] = NULL;
+				//std::cout << gridArray[i] << "\n";
 			}
-			//std::cout << letter << "\n";
-			gridArray[i] = letters[letter];
-			letters[letter] = NULL;
-			//std::cout << gridArray[i] << "\n";
 		}
 	}
 	else if (EDSwitch == 1) {
 		std::cout << "Enter your Grid Code: ";
 		std::cin >> gridCode;
-		for (int i = 0; i < 25; i++) {
-			gridArray[i] = gridCode[i];
+		int gridCodeIter = 0;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				gridArray[i][j] = gridCode[gridCodeIter];
+				gridCodeIter++;
+			}
 		}
 	}
 
@@ -44,7 +51,7 @@ int main() {
 	for (int i = 0; i < 5; i++) {
 		//std::cout << "(" << i + 1 << ")";
 		for (int j = 0; j < 5; j++) {
-			std::cout << gridArray[j + (5 * i)] << " ";
+			std::cout << gridArray[i][j] << " ";
 			//std::cout << gridArray[j + (5 * i)] << "(" << j + 1 << ")" << " ";
 		}
 		std::cout << "\n";
@@ -52,8 +59,9 @@ int main() {
 
 	if (EDSwitch == 0) {
 		std::cout << "\nYour grids' code: ";
-		for (int i = 0; i < 25; i++) {
-			std::cout << gridArray[i];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++)
+			std::cout << gridArray[i][j];
 		}
 	}
 
@@ -81,8 +89,8 @@ int main() {
 		for (int k = 0; k < message.size(); k++) { //k is character in message
 			for (int i = 0; i < 5; i++) { //i = y
 				for (int j = 0; j < 5; j++) { //j = x
-					if (message[k] == gridArray[j + (5 * i)]) {
-						std::cout << "\n" << message[k] << " ";
+					if (message[k] == gridArray[i][j]) {
+						std::cout << "\n" << message[k] << " = ";
 						int l2nX = j + 1;
 						int l2nY = i + 1;
 						std::cout << l2nY << ", " << l2nX;
@@ -92,7 +100,7 @@ int main() {
 				}
 			}
 		}
-		std::cout << "\n";
+		std::cout << "\n\n";
 		for (int i = 0; i < messageSize; i++) {
 			toCrypt[i] = tocryptY[i];
 			//std::cout << codedYArray[i];
@@ -104,47 +112,27 @@ int main() {
 		for (int i = 0; i < messageSize * 2; i++) {
 			std::cout << toCrypt[i];
 		}
-		for (int k = 0; k < messageSize * 2; k++) { //k is character in message
-			for (int i = 0; i < 5; i++) { //i = y
-				for (int j = 0; j < 5; j++) { //j = x
-					if (toCrypt[k] - 1 == j + (5 * i)) {
-						//std::cout << "\n" << toCrypt[k] << ": ";
-						//char n2lX = gridArray[j];
-						int n2lCoordY = i;
-						int n2lCoordX = j;
-						message[floor(k/2)] = gridArray[n2lCoordY + (5 * n2lCoordX)];
-						//std::cout << n2lCoordY;
-					}
-				}
-			}
+
+		std::cout << std::endl << std::endl;
+
+		//Making numbers into letters--------------------------------------------------------------------
+		int iter = 0;
+		for (int k = 0; k < messageSize * 2; k+=2) { //k is character in message
+			std::cout << toCrypt[k] << ", " << toCrypt[k + 1] << " = ";
+			std::cout << gridArray[(toCrypt[k]) - 1][(toCrypt[k + 1]) - 1] << "\n";
+			message[iter] = gridArray[(toCrypt[k]) - 1][(toCrypt[k + 1]) - 1];
+			iter++;
 		}
 		std::cout << "\n";
 	}
+	//------------------------------------------------------------------------------------------------------
 
 	if (EDSwitch == 1) { //decoding
-		for (int k = 0; k < (message.size() / 2); k++) { //k is character in message
-			for (int i = 0; i < 5; i++) { //i = y
-				for (int j = 0; j < 5; j++) { //j = x
-					if (message[k] == i + 1) {
-						std::cout << "\n" << message[k];
-						int l2nY = gridCode[i + 1];
-						//std::cout << l2nY << ", " << l2nX;
-						tocryptY[k] = l2nY;
-					}
-				}
-			}
-		}
-		std::cout << "\n";
-	}
-
-	std::cout << "\n\n";
-
-	/*if (EDSwitch == 0) { //encoding
 		for (int k = 0; k < message.size(); k++) { //k is character in message
 			for (int i = 0; i < 5; i++) { //i = y
 				for (int j = 0; j < 5; j++) { //j = x
-					if (toCrypt[k] == gridArray[j + (5 * i)]) {
-						std::cout << "\n" << toCrypt[k] << " ";
+					if (message[k] == gridArray[i][j]) {
+						std::cout << "\n" << message[k] << " = ";
 						int l2nX = j + 1;
 						int l2nY = i + 1;
 						std::cout << l2nY << ", " << l2nX;
@@ -154,8 +142,26 @@ int main() {
 				}
 			}
 		}
-		std::cout << "\n";
-	} */
+
+		std::cout << "\n\n";
+
+		int iter = 0;
+		for (int k = 0; k < messageSize * 2; k += 2) { //k is character in message
+			toCrypt[k] = tocryptY[iter];
+			toCrypt[k + 1] = tocryptX[iter];
+			iter++;
+		}
+
+		for (int i = 0; i < messageSize * 2; i++) {
+			std::cout << toCrypt[i];
+		}
+
+		for (int k = 0; k < messageSize; k++) {
+			message[k] = gridArray[toCrypt[k] - 1][toCrypt[k + messageSize] - 1];
+		}
+	}
+
+	std::cout << "\n\n";
 
 	if (EDSwitch == 0) {
 		std::cout << "Your coded message: " << message;
@@ -163,14 +169,11 @@ int main() {
 		std::cout << "\n";
 	}
 	else if (EDSwitch == 1) {
-		std::cout << "Your decoded message: ";
-		for (int i = 0; i < messageSize * 2; i++) {
-			std::cout << toCrypt[i];
-		}
+		std::cout << "Your coded message: " << message;
 
 		std::cout << "\n";
 	}
 	
 
-
+	system("PAUSE");
 }
